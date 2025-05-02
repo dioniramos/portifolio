70aee7c524919bc2db5e2d32e4d9a2bd05ae2a65
Dioni Leandro Ramos <76785311+dioniramos@users.noreply.github.com>
2025-05-01 22:43:18 -0300

    update formulario de contato

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,8 +1,17 @@
-<head>
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script> <!-- Font Awesome -->
-</head>
-
 function toggleMenu() {
     const nav = document.getElementById('navbar');
     nav.classList.toggle('active');
 }
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+
+    emailjs.init('XdMX4UNam9_CcIkzJ'); // Substitua por seu User ID do EmailJS
+
+    emailjs.sendForm('service_4tw0k87', 'template_hq0zmhj', this)
+        .then(function () {
+            alert('Mensagem enviada com sucesso!');
+        }, function (error) {
+            alert('Erro ao enviar mensagem: ' + error);
+        });
+});
