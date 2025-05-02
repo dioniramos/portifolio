5493d29aaa0b42ffb358c7e4adbf10073b3ae694
Dioni Leandro Ramos <76785311+dioniramos@users.noreply.github.com>
2025-05-01 21:52:10 -0300

    sem saco para oque escrever aqui

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,3 +1,7 @@
+<head>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script> <!-- Font Awesome -->
+</head>
+
 function toggleMenu() {
     const nav = document.getElementById('navbar');
     nav.classList.toggle('active');
